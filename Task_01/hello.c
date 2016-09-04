7bad286be30cae330004c837e0e70bbf16190951
Chinmay Nivsarkar <mit.chinmayn@gmail.com>
2016-09-05 00:41:54 +0530

    Task 01 files,only source.

---
+++ Task_01/hello.c
@@ -0,0 +1,17 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int __init hello_module_init(void)
+{
+    printk(KERN_DEBUG "Hello World");
+    return 0;
+}
+
+static void __exit hello_cleanup(void)
+{
+    printk(KERN_DEBUG "Module by Chinmay Nivsarkar<mit.chinmayn@gmail.com>");
+}
+
+module_init(hello_module_init);
+module_exit(hello_cleanup);
