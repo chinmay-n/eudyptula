d1bf9b5fb9bcf2ea4969c9c96f13d88212151dd2
Chinmay Nivsarkar <mit.chinmayn@gmail.com>
2016-09-06 14:04:17 +0530

    Split line to avoid possible unwrapped commit description
    Signed-off-by: Chinmay Nivsarkar <mit.chinmayn@gmail.com>

--- Task_04/hello.c
+++ Task_04/hello.c
@@ -2,16 +2,14 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-static int __init hello_module_init(void)
-{
-    printk(KERN_DEBUG "Hello World");
-    return 0;
+static int __init hello_module_init(void){
+	printk(KERN_DEBUG "Hello World");
+	return 0;
 }
 
-static void __exit hello_cleanup(void)
-{
-    printk(KERN_DEBUG "Module by Chinmay 
-	Nivsarkar<mit.chinmayn@gmail.com>");
+static void __exit hello_cleanup(void){
+	printk(KERN_DEBUG "Module by Chinmay 
+		Nivsarkar<mit.chinmayn@gmail.com>");
 }
 
 module_init(hello_module_init);
