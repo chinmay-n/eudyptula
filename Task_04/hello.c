3875fee2f1876bea9a3aff518bf9f6b89648b5bf
chinmay-n <nivsarkar.chinmay@learner.manipal.edu>
2016-09-07 10:37:05 +0530

    Added Module License,Desc and Author

--- Task_04/hello.c
+++ Task_04/hello.c
@@ -16,3 +16,7 @@ static void __exit hello_cleanup(void)
 
 module_init(hello_module_init);
 module_exit(hello_cleanup);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Chinmay Nivsarkar<mit.chinmayn@gmail.com>");
+MODULE_DESCRIPTION("Hello World module");
