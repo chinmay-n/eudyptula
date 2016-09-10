757e7e3bbc6e7656b0ed69eed14497947ffbbedb
Chinmay Nivsarkar <mit.chinmayn@gmail.com>
2016-09-11 01:05:57 +0530

    Submission retried, checkpatch.pl warnings eliminated in coding_style.c

--- Task_04/hello.c
+++ Task_04/hello.c
@@ -16,7 +16,3 @@ static void __exit hello_cleanup(void)
 
 module_init(hello_module_init);
 module_exit(hello_cleanup);
-
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Chinmay Nivsarkar<mit.chinmayn@gmail.com>");
-MODULE_DESCRIPTION("Hello World module");
